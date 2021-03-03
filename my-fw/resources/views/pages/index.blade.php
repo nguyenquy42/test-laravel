2aa091cfbe14d55b251897ac1ee1c824559a2761
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-03-03 11:29:03 +0700

    up cade giao diện trang home 100%

--- my-fw/resources/views/pages/index.blade.php
+++ my-fw/resources/views/pages/index.blade.php
@@ -4,16 +4,106 @@
 
 
 @section('content')
+<main>
 
-<h1>đây là trang chủ user use</h1>
-<div class="btn-group">
-              <button type="button" class="btn btn-primary">minh quys</button>
-              <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
-              </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Link 1</a>
-                <a class="dropdown-item" href="#">Link 2</a>
+  <!-- List Product Start -->
+  <section class="section_product">
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="title text-center mt-5 mb-5">
+          <h2>Danh sách sản phẩm</h2>
+          <span>cách sản phầm có thể bạn muốn biết</span>
+        </div>
+      </div>
+      <div class="row">
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+          <div class="col-lg-3">
+            <div class="list_product mb-4">
+              <div class="product_item">
+                <div class="product_item_img">
+                  <a href="#"><img src="assets/images/product/1.jpg" alt="product"></a>
+                </div>
+                <div class="product_item_content">
+                  <span>Công nghệ</span>
+                  <a href="#">
+                    <h3 class="product_item_title">Sách củ nhất</h3>
+                  </a>
+                  <p class="product_item_intro">sản phẩm lỗi, mua ngu rán chiệu.</p>
+                  <div class="star_rating">
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star checked"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                  </div>
+                  <div class="list_price_cart">
+                    <div class="price">
+                      <span>5.000 $</span>
+                    </div>
+                    <ul class="m-0 none-list-type product-cart">
+                      <li class="product_cart_item"><a href="#"><i class="fa fa-heart"></i></a></li>
+                      <li class="product_cart_item"><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        <?php endfor; ?>
+      </div>
+
+    </div>
+
+  </section>
+  <!-- List Product End -->
+
+  <!-- List Blog Start -->
+  <section class="section_blog">
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="title text-center mt-5 mb-5">
+          <h2>Bài viết mơi nhất</h2>
+          <span>Nếu muốn thông minh biết thêm nhiều thông tin thì mày nên đọc các bài viết dưới đây, nhớ follow để không bỏ lởi bất kỳ bài viết mới nhất nào của tao.</span>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <?php for ($i = 0; $i < 3; $i++) : ?>
+          <div class="col-lg-4">
+            <div class="row mb-4 main_blog">
+              <div class="col-lg-3  d-flex align-items-center">
+                <div class="blog_item_img">
+                  <a href="#"><img src="assets/images/product/2.jpg" alt="img"></a>
+                </div>
+              </div>
+              <div class="col-lg-9">
+                <div class="blog_item_content">
+                  <a href="#">
+                    <h3 class="blog_item_title">Bí kiếp CONAN</h3>
+                  </a>
+                  <p class="m-0 blog_item_intro">Hướng dẫn cách có người yêu, một phát ăn ngay và luôn, không tin thì thôi. NEXT..</p>
+                  <div class="list_author_date">
+                    <ul class="none-list-type">
+                      <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i> Minh Quang</a></li>
+                      <li><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i> 3/3/2021</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endfor; ?>
+
+      </div>
+    </div>
+
+  </section>
+  <!-- List Blog End -->
+
+
+</main>
 
 @endsection
---
+++ my-fw/resources/views/pages/partials/footer.blade.php
@@ -0,0 +1,50 @@
+<!-- Footer Start -->
+
+<footer class="main_footer bg_black">
+  <div class="container-fluid text-white">
+    <div class="row">
+
+      <div class="col-lg-4">
+        <div class="main_footer_item">
+          <div class="text-center">
+            <div class="footer_img">
+              <a href="#"><img src="assets/images/logo/logo-header.png" alt="logo"></a>
+            </div>
+            <p>Đây là trang đang test thử, trong quá trinh mà mài xài có hư thì liên hệ tao qua <a href="#">quy97@gmail.com</a>. Nên nhắn tin nói chuyện lịch sự, chân ái cảm ơn.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <div class="main_footer_item text-center">
+          <h3 class="footer_title mb-3">Mẹo Vặt</h3>
+          <ul class="none-list-type text-left">
+            <li><a href="#">Hút thuốc bắng mắt</a></li>
+            <li><a href="#">Ăn cứt mũi thông minh</a></li>
+            <li><a href="#">Đi ia quên đêm giấy</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-4">
+        <div class="main_footer_item text-center">
+          <h3 class="footer_title mb-3">Danh Mục</h3>
+          <ul class="none-list-type text-left">
+            <li><a href="#">Mua đồ giá mắt</a></li>
+            <li><a href="#">Mua đồ củ không Ngu</a></li>
+            <li><a href="#">kiến thức cho mấy đứa ế</a></li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="contact_footer_bottom">
+        <p class="m-0">Được thiết kế và phát tiển bở <a href="#">Minh Quý</a> của cty TMĐT Công Nghệ<a href="https://lptech.asia"> LPTech. </a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- Footer End -->
