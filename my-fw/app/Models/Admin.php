98f7ef8c30873a119861cad4c77595b4ddd10015
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-03-08 11:58:31 +0700

    up models admins and users

--- my-fw/app/Models/Admin.php
+++ my-fw/app/Models/Admin.php
@@ -2,20 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
-  use Notifiable;
+    use HasFactory, Notifiable;
 
-  protected $guard = 'admin';
+    protected $table = 'admins';
 
-  protected $fillable = [
-    'name', 'email', 'password',
-  ];
+    protected $guarded = 'admin';
 
-  protected $hidden = [
-    'password', 'remember_token',
-  ];
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
